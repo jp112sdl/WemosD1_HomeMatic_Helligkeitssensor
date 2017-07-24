bba4bd5be58b5681a785e8518eafbd1b69c65f12
jp112sdl <jp112sdl@icloud.com>
2017-07-24 11:51:57 +0200

    added Images

--- WemosD1_HomeMatic_Helligkeitssensor.ino
+++ WemosD1_HomeMatic_Helligkeitssensor.ino
@@ -187,12 +187,12 @@ void saveConfigCallback () {
 
 void parseBytes(const char* str, char sep, byte* bytes, int maxBytes, int base) {
   for (int i = 0; i < maxBytes; i++) {
-    bytes[i] = strtoul(str, NULL, base);  // Convert byte
-    str = strchr(str, sep);               // Find next separator
+    bytes[i] = strtoul(str, NULL, base); 
+    str = strchr(str, sep);               
     if (str == NULL || *str == '\0') {
-      break;                            // No more separators, exit
+      break;                           
     }
-    str++;                                // Point to next character after separator
+    str++;                               
   }
 }
 
